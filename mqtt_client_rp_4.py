b8de8f60ef959c0ef8f932fcd5b7c0134c27fbf2
schauppi <46676413+schauppi@users.noreply.github.com>
2021-03-11 19:28:25 +0100

    enabled communication

--- mqtt_client_rp_4.py
+++ mqtt_client_rp_4.py
@@ -2,27 +2,32 @@ import paho.mqtt.client as mqtt
 import time
 
 def on_message(client, userdata, message):
-	print("message received", str(message.payload.decode("utf-8")))
-	
-	
-def on_subscribe(client, userdata, message, granted_qos):
-	print(message)
-	
+    print("message received " ,str(message.payload.decode("utf-8")))
+    #print("message topic=",message.topic)
+    #print("message qos=",message.qos)
+    #print("message retain flag=",message.retain)
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connected OK Returned code=",rc)
+    else:
+        print("Bad connection Returned code=",rc)
+
 broker_address="broker.mqttdashboard.com"
 
-client = mqtt.Client("Client2")
+client = mqtt.Client("Client1")
 
 client.on_message=on_message
-client.on_subscribe=on_subscribe
-
-print("connecting to broker")
+client.on_connect=on_connect
 
 client.connect(broker_address)
 
 client.loop_start()
 
-client.subscribe("hydro/temp",0)
+while True:
 
-time.sleep(4)
+	client.subscribe("hydro/temp")
+
+	client.subscribe("hydro/ph")
 
 client.loop_stop()
--- mqtt_client_rp_zero.py
+++ mqtt_client_rp_zero.py
@@ -2,34 +2,38 @@ import paho.mqtt.client as mqtt
 import time
 
 def on_message(client, userdata, message):
-	print("message received " ,str(message.payload.decode("utf-8")))
-	
+    print("message received " ,str(message.payload.decode("utf-8")))
+    #print("message topic=",message.topic)
+    #print("message qos=",message.qos)
+    #print("message retain flag=",message.retain)
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connected OK Returned code=",rc)
+    else:
+        print("Bad connection Returned code=",rc)
+
 broker_address="broker.mqttdashboard.com"
 
-client = mqtt.Client("Client1")
+client = mqtt.Client("Client2")
 
 client.on_message=on_message
-
-print("connecting to broker")
+client.on_connect=on_connect
 
 client.connect(broker_address)
 
 client.loop_start()
 
-client.subscribe("hydro/temp")
+for i in range(3):
 
-client.publish("hydro/temp", "21 Grad")
+	client.publish("hydro/temp", "{} Grad".format(i))
 
-"""
-client.subscribe("FH/test")
+	client.publish("hydro/ph", "{} ph".format(i))
 
-client.publish("FH/test", "xoxo")
-
-client.subscribe("topic/fh/test")"""
-
-time.sleep(4)
+	time.sleep(4)
 
 client.loop_stop()
+
 
 
 
