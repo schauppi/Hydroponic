de2064b1a8ed3ea4badc5ffab1e49569dca6cd03
schauppi <schauppdavid@gmail.com>
2021-04-26 10:09:24 +0200

    sensors

--- Sensors_Actuators/sensors_actuators.py
+++ Sensors_Actuators/sensors_actuators.py
@@ -2,6 +2,8 @@ from AtlasI2C import (AtlasI2C)
 import time
 import ph_ec_helper
 import paho.mqtt.client as mqtt
+import time
+import RPi.GPIO as GPIO
 
 #get i2c devices from ph_ec_helper module
 #ph 0x63 / 99 i2c
@@ -9,19 +11,94 @@ import paho.mqtt.client as mqtt
 device_list = ph_ec_helper.get_devices()
 device = device_list
 
+GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
+RELAIS_lamp = 24 #Lampe
+RELAIS_water_pump = 18 #Wasserpumpe
+RELAIS_air_pump = 23 #Luftpumpe
+
+RELAIS_dosing_pump_4 = 21 #Dosierpumpe4(innen)
+RELAIS_dosing_pump_3 = 20 #Dosierpumpe3
+RELAIS_dosing_pump_2 = 26 #Dosierpumpe2
+RELAIS_dosing_pump_1 = 19 #Dosierpumpe1(aussen)
+
+GPIO.setup(RELAIS_lamp, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.setup(RELAIS_water_pump, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.setup(RELAIS_air_pump, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.setup(RELAIS_dosing_pump_4, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.setup(RELAIS_dosing_pump_3, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.setup(RELAIS_dosing_pump_2, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.setup(RELAIS_dosing_pump_1, GPIO.OUT) # GPIO Modus zuweisen
+
+
 def on_connect(client, userdata, flags, rc):
 	if rc==0:
 		print("connection ok", rc)
 	else:
 		print("connection error", rc)
+		
+def on_message(client, userdata, message):
+	print("message received " ,str(message.payload.decode("utf-8")))
+	
+	#lamp, air/water pump
+	if message.topic == "hydro/lamp/on":
+		GPIO.output(RELAIS_lamp, True)
+	elif message.topic == "hydro/lamp/off":
+		GPIO.output(RELAIS_lamp, False)
+		
+	if message.topic == "hydro/water_pump/on":
+		GPIO.output(RELAIS_water_pump, True)
+	elif message.topic == "hydro/water_pump/off":
+		GPIO.output(RELAIS_water_pump, False)
+		
+	if message.topic == "hydro/air_pump/on":
+		GPIO.output(RELAIS_air_pump, True)
+	elif message.topic == "hydro/air_pump/off":
+		GPIO.output(RELAIS_air_pump, False)
+		
+	#dosing pumps
+	if message.topic == "hydro/dosing_pump_1/on":
+		GPIO.output(RELAIS_dosing_pump_1, False)
+	elif message.topic == "hydro/dosing_pump_1/off":
+		GPIO.output(RELAIS_dosing_pump_1, True)
+		
+	if message.topic == "hydro/dosing_pump_2/on":
+		GPIO.output(RELAIS_dosing_pump_2, False)
+	elif message.topic == "hydro/dosing_pump_2/off":
+		GPIO.output(RELAIS_dosing_pump_2, True)
+		
+	if message.topic == "hydro/dosing_pump_3/on":
+		GPIO.output(RELAIS_dosing_pump_3, False)
+	elif message.topic == "hydro/dosing_pump_3/off":
+		GPIO.output(RELAIS_dosing_pump_3, True)
+		
+	if message.topic == "hydro/dosing_pump_4/on":
+		GPIO.output(RELAIS_dosing_pump_4, False)
+	elif message.topic == "hydro/dosing_pump_4/off":
+		GPIO.output(RELAIS_dosing_pump_4, True)
 
 #Mqtt Standard procedure
 broker_address = "192.168.8.166"
 client = mqtt.Client("Sensors_Actuators")
 client.on_connect=on_connect
+client.on_message=on_message
 client.connect(broker_address)
 client.loop_start()
-	
+
+#topic subscriptions
+client.subscribe("hydro/lamp/on")
+client.subscribe("hydro/lamp/off")
+client.subscribe("hydro/water_pump/on")
+client.subscribe("hydro/water_pump/off")
+client.subscribe("hydro/air_pump/on")
+client.subscribe("hydro/air_pump/off")
+client.subscribe("hydro/dosing_pump_1/on")
+client.subscribe("hydro/dosing_pump_1/off")
+client.subscribe("hydro/dosing_pump_2/on")
+client.subscribe("hydro/dosing_pump_2/off")
+client.subscribe("hydro/dosing_pump_3/on")
+client.subscribe("hydro/dosing_pump_3/off")
+client.subscribe("hydro/dosing_pump_4/on")
+client.subscribe("hydro/dosing_pump_4/off")
 
 while True:
 	#measure ph and ec from ph_ec_helper module
